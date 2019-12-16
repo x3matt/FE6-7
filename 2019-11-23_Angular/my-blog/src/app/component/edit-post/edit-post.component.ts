785dbc31172d97744b8fb9115462f191e41da70c
Vladislav <x3matt21@mail.ru>
2019-12-16 23:30:30 +0100

    ClassWork

---
+++ 2019-11-23_Angular/my-blog/src/app/component/edit-post/edit-post.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {PostService} from '../../service/post/post.service';
+
+@Component({
+  selector: 'app-edit-post',
+  templateUrl: './edit-post.component.html',
+  styleUrls: ['./edit-post.component.scss']
+})
+export class EditPostComponent implements OnInit {
+
+  private postForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private postService: PostService) { }
+
+  ngOnInit() {
+    this.postForm = this.fb.group({
+      id: [],
+      title: [''],
+      body: [''],
+      date: ['']
+    });
+  }
+
+  onSubmit($event) {
+    console.log(this.postForm.getRawValue());
+    this.postService.savePost(this.postForm.getRawValue()).subscribe(
+      result => console.log(result)
+    );
+}
+}
